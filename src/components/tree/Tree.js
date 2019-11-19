256c2dd6429c46a25a2f7fb7debfff5216a4c9b6
Grimes <crameg@amazon.com>
2019-11-19 17:39:20 -0500

    only increase

---
+++ src/components/tree/Tree.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './Tree.css'
+
+class Tree extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            berries: 0
+        }
+
+    }
+
+    clickTree = event => {
+
+        this.setState({
+            berries: this.state.berries + 1
+        })
+    }
+
+    render(){
+        return(
+            <div 
+                className="Tree"
+                
+            >
+                <div
+                    className="trImage"
+                    onClick={this.clickTree}
+                >
+                </div>
+                <div>
+                    {this.state.berries}
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Tree;
